07ab44a1f395517fefde47329b2be26a9c8cd3a3
Bas van Summeren <basvansummeren@hotmail.com>
2015-11-19 13:35:28 +0100

    Fix overwrite of the main

--- Visual Studio/RobotArminator/RobotArminator/RobotArminator.cpp
+++ Visual Studio/RobotArminator/RobotArminator/RobotArminator.cpp
@@ -5,23 +5,12 @@
 #include <ctime>
 #include "stdafx.h"
 #include "Controller.hpp"
-#include "BallPositionCalculator.hpp"
-#include "VisionPosition.hpp"
 
 using namespace RobotArminator;
 
 int main(int argc, char* argv[])
 {
-	BallPositionCalculator ballPositionCalculator;
-	RobotArminator::VisionPosition visionPositionSideView = { 1000, 200, time_t(0.1), UP };
-	RobotArminator::VisionPosition visionPositionTopView = { 1000, 300, time_t(0.1), SIDE };
-
-	ballPositionCalculator.startPositionCalculation();
-	ballPositionCalculator.calculateTwoPositionToOnePosition(visionPositionSideView, visionPositionTopView);
-
-	//wait for input
-	std::cin.get();    
-	
-	return 0;
+    Controller controller;
+    return 0;
 }
 
