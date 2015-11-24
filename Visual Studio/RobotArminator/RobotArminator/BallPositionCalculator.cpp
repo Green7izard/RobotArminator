8b061d09478ea61d674d998370c13f506cddce62
Remco van Alen <remcovanalen@hotmail.com>
2015-11-24 16:26:05 +0100

    Parabool functie toegevoegd. De twee x coordinaten kunnen worden berekend op y = 0

--- Visual Studio/RobotArminator/RobotArminator/BallPositionCalculator.cpp
+++ Visual Studio/RobotArminator/RobotArminator/BallPositionCalculator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 #include "BallPositionCalculator.hpp"
 #include "Trajectory.hpp"
 #include "Vector.hpp"
@@ -30,20 +31,37 @@ namespace BallPosition
 	void BallPositionCalculator::calculateTraject(VisionPosition sideView, VisionPosition topView)
 	{
 		calculateLiniairTraject(topView);
+		calculateCircleTraject(sideView);
 	}
 
 	void BallPositionCalculator::calculateLiniairTraject(VisionPosition pos)
 	{
 		float multiplier = (float)(pos.Y - lastTopPosition.Y) / (pos.X - lastTopPosition.X);
 		int startValue = lastTopPosition.Y - (lastTopPosition.X * multiplier);
-		int newY = (multiplier * 2740) + startValue;
-		std::cout << multiplier << " * " << "2740" << " + " << startValue << " = " << newY << std::endl;
+		int newY = (multiplier * tableWidth) + startValue;
+		std::cout << multiplier << " * " << tableWidth << " + " << startValue << " = " << newY << std::endl;
 
 	}
 
 	void BallPositionCalculator::calculateCircleTraject(VisionPosition pos)
 	{
+		//http://www.hhofstede.nl/modules/projectielbanen.htm
+		
+		float corner = 20; //degrese
+		float height = 0.2; //meters
+		float speed = 5; //meter/seconds
 
+		float PI = 3.14159265358979;
+		float a = (-9.81 / (2 * speed * speed)) * (tan(corner * PI / 180) + 1);
+		float b = tan(corner * PI / 180);
+		float c = height;
+
+		//y = (b * x) + (a * x^2) + c;
+		float xPlus = (-b + sqrt((b*b) - (4 * a * c))) / (2 * a);
+		float xMin = (-b - sqrt((b*b) - (4 * a * c))) / (2 * a);
+
+		std::cout << xPlus;
+		std::cout << xMin;
 	}
 
 
