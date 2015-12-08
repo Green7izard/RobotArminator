b04714415a4d7f34876820259a7fc645cf131bef
Paul <p.verhoeven@live.nl>
2015-12-08 13:01:54 +0100

    added PFC vision to main

--- Visual Studio/RobotArminator/RobotArminator/RobotArminator.cpp
+++ Visual Studio/RobotArminator/RobotArminator/RobotArminator.cpp
@@ -6,6 +6,7 @@
 #include "opencv2/opencv.hpp"
 #include "Camera.hpp"
 #include "HCDVision.hpp"
+#include "PFCVision.hpp"
 #include "Orientation.hpp"
 
 
@@ -13,30 +14,38 @@ int main(int argc, char* argv[])
 {
     char * win= "window";
     
+	//Camera aanmaken.
     Vision::Camera cam(0);
     cam.setSize(1280, 720);
     Vision::Position2D pos;
-    std::cout << "Start Camera with size " <<cam.getWidth()<<"x"<< cam.getHeight()<<"\n";
+	std::cout << "Start Camera with size " << cam.getWidth() << "x" << cam.getHeight() << std::endl;
     cv::Mat cameraFrame;
-    Vision::ColorHCDVision HCD(RobotArminator::TOP, &cam);
-    std::cout << "Frame Created\n";
+	//Tracking aanmaken.
+    //Vision::ColorHCDVision HCD(RobotArminator::TOP, &cam);
+	Vision::PFCVision PFC(RobotArminator::TOP, &cam);
+	//Window aanmaken.
+	cv::namedWindow(win, cv::WINDOW_AUTOSIZE);
 
-    cv::namedWindow(win, cv::WINDOW_AUTOSIZE);
-    while (true) {
-        cam.getCurrentImage(cameraFrame);
-        cv::Size s = cameraFrame.size();
-        //std::cout << "Got Image\n";
-        //std::cout << s.width << "x" << s.height << "\n";
-        if (HCD.locateObject(cameraFrame, pos))
-        {
-            std::cout << "Found at: " << pos.X << "," << pos.Y << "\n";
-        }
-        else
-        {
-            std::cout << "Could not find the ball... Last at: " << pos.X << "," << pos.Y << "\n";
-        }
-        imshow(win, cameraFrame);
-        //std::cout << "Shown the data\n";
+	while (true) {
+		cam.getCurrentImage(cameraFrame);
+		//cv::Size s = cameraFrame.size();
+		//std::cout << "Got Image\n";
+		//std::cout << s.width << "x" << s.height << "\n";
+
+		//Object tracking
+		if (PFC.locateObject(cameraFrame, pos))
+			//if (HCD.locateObject(cameraFrame, pos))
+		{
+			std::cout << "Found at: " << pos.X << "," << pos.Y << std::endl;
+		}
+		else
+		{
+			std::cout << "Could not find the ball... Last at: " << pos.X << "," << pos.Y << std::endl;
+		}
+		//if debug() //Only show tracking output when in debug mode.
+		//imshow(win, cameraFrame);
+
+
         if (cv::waitKey(1) >= 0) break;
     }
     return 0;
