788ebe265a67ad77a277cf766f3f6ef38360d6da
Bas van Summeren <basvansummeren@hotmail.com>
2015-12-14 12:48:12 +0100

    Reset CV main

--- Visual Studio/RobotArminator/RobotArminator/RobotArminator.cpp
+++ Visual Studio/RobotArminator/RobotArminator/RobotArminator.cpp
@@ -1,23 +1,49 @@
 // RobotArminator.cpp : Defines the entry point for the console application.
 //
-#include "stdafx.h"
-#include "Controller.hpp"
-#include "BallPositionCalculator.hpp"
-#include <chrono>
 #include <iostream>
-#include <ctime>
-using namespace RobotArminator;
-using namespace BallPosition;
+#include "stdafx.h"
+#include "opencv2/opencv.hpp"
+#include "Camera.hpp"
+#include "HCDVision.hpp"
+#include "PFCVision.hpp"
+#include "Orientation.hpp"
+
 
 int main(int argc, char* argv[])
 {
-    Controller t();
+    char * win = "window";
 
+    //Camera aanmaken.
+    Vision::Camera cam(0);
+    cam.setSize(1280, 720);
+    Vision::Position2D pos;
+    std::cout << "Start Camera with size " << cam.getWidth() << "x" << cam.getHeight() << std::endl;
+    cv::Mat cameraFrame;
+    //Vision::PFCVision HCD(RobotArminator::TOP, &cam);
+    //Vision::CannyHCDVision HCD(RobotArminator::TOP, &cam);
+    Vision::ColorHCDVision HCD(RobotArminator::SIDE, &cam);
+    std::cout << "Frame Created\n";
 
-
-	//wait for input
-	std::cin.get(); 
-    //Controller controller;
+    cv::namedWindow(win, cv::WINDOW_AUTOSIZE);
+    while (true) {
+        cam.getCurrentImage(cameraFrame);
+        cv::Size s = cameraFrame.size();
+        //std::cout << "Got Image\n";
+        //std::cout << s.width << "x" << s.height << "\n";
+        if (HCD.locateObject(cameraFrame, pos))
+        {
+            std::cout << "Found at: " << pos.X << "," << pos.Y << std::endl;
+            RobotArminator::VisionPosition newPos = HCD.convertToCoordinate(pos, RobotArminator::Clock::universal_time(), s.width, s.height);
+            std::cout << "Converted to: " << newPos.X << "," << newPos.Y << std::endl;
+        }
+        else
+        {
+            std::cout << "Could not find the ball... Last at: " << pos.X << "," << pos.Y << std::endl;
+        }
+        imshow(win, cameraFrame);
+        //std::cout << "Shown the data\n";
+        if (cv::waitKey(1) >= 0) break;
+    }
     return 0;
 }
 
