32c077662aea8e5292e97e8dca308672560bf995
Bas van Summeren <basvansummeren@hotmail.com>
2016-01-05 09:51:43 +0100

    Its a main! it sends stuff!

--- Visual Studio/Workshop/DemoArminator/DemoArminator.cpp
+++ Visual Studio/Workshop/DemoArminator/DemoArminator.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
+#include "TCPRobot.hpp"
 
+using namespace std;
+using namespace Robot;
 
 int main()
 {
+    Robot::TCPRobot robot;
+
+    string command;
+    do {
+        cin >> command;
+        robot.sendMessage(command);
+    } while (command.compare("quit") != 0);
     return 0;
 }
 
