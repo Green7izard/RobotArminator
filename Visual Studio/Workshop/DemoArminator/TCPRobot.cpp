11045e74ff011f48ff142cf1b31d7e81d14317f2
Bas van Summeren <basvansummeren@hotmail.com>
2016-01-05 09:47:29 +0100

    Basic class for the sending and receiving

---
+++ Visual Studio/Workshop/DemoArminator/TCPRobot.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include "TCPRobot.hpp"
+
+namespace Robot
+{
+    TCPRobot::TCPRobot(std::string ipAdress, int port)
+    {
+
+        //Start up Winsock…
+        WSADATA wsadata;
+
+        int error = WSAStartup(0x0202, &wsadata);
+
+        //Did something happen?
+        if (error) {
+            throw new std::ios_base::failure("WSA Failed");
+        }
+
+        //Socket address information
+        SOCKADDR_IN target;
+        // address family Internet
+        target.sin_family = AF_INET;
+        //Port to connect on
+        target.sin_port = htons(port);
+        //Target IP
+        target.sin_addr.s_addr = inet_addr(ipAdress.c_str());
+
+        connection = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //Create socket
+
+        if (connect(connection, (SOCKADDR *)&target, sizeof(target)) == SOCKET_ERROR)
+        {
+            //Couldn't connect
+            throw new std::ios_base::failure("Could not connect to target");
+        }
+    }
+
+
+    TCPRobot::~TCPRobot()
+    {
+        if (connection)
+        {
+            closesocket(connection);
+        }
+        WSACleanup(); //Clean up Winsock
+    }
+
+    bool Robot::TCPRobot::sendMessage(std::string message)
+    {
+        return send(connection, message.c_str(), static_cast<int>(message.length()), 0) > 0;
+    }
+
+    std::string TCPRobot::readMessage() {
+        char buffer[128];
+        int bytesReceived = recv(connection, buffer, sizeof(buffer), 0);
+        return std::string(buffer);
+    }
+}
---
+++ Visual Studio/Workshop/DemoArminator/TCPRobot.hpp
@@ -0,0 +1,25 @@
+#pragma once 
+#pragma comment(lib, "Ws2_32.lib")
+
+#include "stdafx.h"
+#include <winsock.h>
+
+namespace Robot
+{
+    class TCPRobot
+    {
+    private:
+        SOCKET connection;
+    public:
+        //Construct
+        TCPRobot(std::string ipAdress = "127.0.0.1", int port = 63258);
+        //Close the connection
+        virtual ~TCPRobot();
+
+        //Send a message
+        bool sendMessage(std::string message);
+        //Try to read a message
+        std::string readMessage();
+    };
+}
+
